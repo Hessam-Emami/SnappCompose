70d779e42d87a13286106b1e2183c3d93ab88fa5
Hessam Emami <hesam.emami9@gmail.com>
2020-09-19 23:40:54 +0430

    Setup google maps

---
+++ app/src/main/java/com/emami/snappcompose/MapViewUtils.kt
@@ -0,0 +1,60 @@
+package com.emami.snappcompose
+
+import android.os.Bundle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.onCommit
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.platform.LifecycleOwnerAmbient
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.android.libraries.maps.MapView
+
+/**
+ * Remembers a MapView and gives it the lifecycle of the current LifecycleOwner
+ */
+@Composable
+fun rememberMapViewWithLifecycle(): MapView {
+    val context = ContextAmbient.current
+    val mapView = remember {
+        MapView(context).apply {
+            id = R.id.map
+        }
+    }
+
+    // Makes MapView follow the lifecycle of this composable
+    val lifecycleObserver = rememberMapLifecycleObserver(mapView)
+    val lifecycle = LifecycleOwnerAmbient.current.lifecycle
+    onCommit(lifecycle) {
+        lifecycle.addObserver(lifecycleObserver)
+        onDispose {
+            lifecycle.removeObserver(lifecycleObserver)
+        }
+    }
+
+    return mapView
+}
+
+@Composable
+private fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
+    remember(mapView) {
+        LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+                Lifecycle.Event.ON_START -> mapView.onStart()
+                Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                Lifecycle.Event.ON_STOP -> mapView.onStop()
+                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+                else -> throw IllegalStateException()
+            }
+        }
+    }
+
+//fun GoogleMap.setZoom(
+//    @FloatRange(from = MinZoom.toDouble(), to = MaxZoom.toDouble()) zoom: Float
+//) {
+//    resetMinMaxZoomPreference()
+//    setMinZoomPreference(zoom)
+//    setMaxZoomPreference(zoom)
+//}
