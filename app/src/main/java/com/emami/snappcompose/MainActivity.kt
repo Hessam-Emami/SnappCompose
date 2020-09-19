3b676cb30e51f430363c0dbe93abcdf6932ec527
Hessam Emami <hesam.emami9@gmail.com>
2020-09-20 00:03:41 +0430

    Create simple HomeScreen which hosts a map

--- app/src/main/java/com/emami/snappcompose/MainActivity.kt
+++ app/src/main/java/com/emami/snappcompose/MainActivity.kt
@@ -2,37 +2,44 @@ package com.emami.snappcompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.ui.tooling.preview.Preview
 import com.emami.snappcompose.ui.SnappComposeTheme
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.model.LatLng
+import com.google.android.libraries.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SnappComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen() {
+    val map = rememberMapViewWithLifecycle()
+    AndroidView({ map }) { mapView ->
+        mapView.getMapAsync {
+            val position = LatLng("40.416775".toDouble(), "-3.703790".toDouble())
+            it.addMarker(
+                MarkerOptions().position(position)
+            )
+            it.moveCamera(CameraUpdateFactory.newLatLng(position))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SnappComposeTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
