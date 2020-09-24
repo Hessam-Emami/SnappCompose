a59b875ab9ca44c3b10df6558faa40d5bfee625a
Hessam Emami <hesam.emami9@gmail.com>
2020-09-24 19:54:13 +0330

    Implement basic MapPointer composable

--- app/src/main/java/com/emami/snappcompose/MainActivity.kt
+++ app/src/main/java/com/emami/snappcompose/MainActivity.kt
@@ -2,8 +2,17 @@ package com.emami.snappcompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Stack
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.FixedScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.ui.tooling.preview.Preview
 import com.emami.snappcompose.ui.SnappComposeTheme
@@ -25,13 +34,44 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun HomeScreen() {
     val map = rememberMapViewWithLifecycle()
-    AndroidView({ map }) { mapView ->
-        mapView.getMapAsync {
-            val position = LatLng("40.416775".toDouble(), "-3.703790".toDouble())
-            it.addMarker(
-                MarkerOptions().position(position)
-            )
-            it.moveCamera(CameraUpdateFactory.newLatLng(position))
+    //Location of Tehran- Iran
+    val position = LatLng("35.6892".toDouble(), "51.3890".toDouble())
+    Stack {
+        AndroidView({ map }) { mapView ->
+            mapView.getMapAsync {
+                it.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        position, 15.0f
+                    )
+                )
+
+            }
+        }
+        MapPointer(Modifier.gravity(Alignment.Center)) {
+            map.getMapAsync {
+                val target = it.cameraPosition.target
+                it.addMarker(MarkerOptions().position(target))
+            }
+        }
+    }
+}
+
+@Composable
+fun MapPointer(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Image(
+        asset = imageResource(id = R.drawable.ic_location_start_pointer),
+        modifier = modifier
+            .clickable(onClick = onClick).padding(16.dp), contentScale = FixedScale(.8f)
+
+    )
+
+}
+
+@Preview
+@Composable
+fun MapPointerPreview() {
+    SnappComposeTheme {
+        MapPointer {
         }
     }
 }
@@ -40,6 +80,6 @@ fun HomeScreen() {
 @Composable
 fun DefaultPreview() {
     SnappComposeTheme {
-        HomeScreen()
+//        HomeScreen()
     }
 }
