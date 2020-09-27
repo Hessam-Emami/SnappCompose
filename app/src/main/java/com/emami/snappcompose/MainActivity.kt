a21bb4b12349456c1ca42c47833e66516f1db009
Hessam Emami <hesam.emami9@gmail.com>
2020-09-28 00:52:23 +0330

    Implement simple mechanism to animate the mapPointer on the fly

--- app/src/main/java/com/emami/snappcompose/MainActivity.kt
+++ app/src/main/java/com/emami/snappcompose/MainActivity.kt
@@ -2,6 +2,11 @@ package com.emami.snappcompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.DpPropKey
+import androidx.compose.animation.core.TransitionState
+import androidx.compose.animation.core.transitionDefinition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.transition
 import androidx.compose.foundation.Box
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -9,7 +14,7 @@ import androidx.compose.foundation.layout.Stack
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,6 +43,8 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun HomeScreen() {
     val map = rememberMapViewWithLifecycle()
+    val buttonState = remember { mutableStateOf(MapPointerState.IDLE) }
+
     //Location of Tehran- Iran
     val position = LatLng("35.6892".toDouble(), "51.3890".toDouble())
     Stack {
@@ -51,13 +58,21 @@ fun HomeScreen() {
             }
         }
         //TODO add marker animation
-        MapPointer(Modifier.gravity(Alignment.Center).padding(bottom = 52.dp)) {
+        AnimatedMapPointer(
+            modifier = Modifier.gravity(Alignment.Center).padding(bottom = 52.dp), buttonState
+        ) {
             map.getMapAsync {
                 val target = it.cameraPosition.target
                 it.addMarker(
                     MarkerOptions().position(target)
                         .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_marker_start))
                 )
+                it.setOnCameraMoveStartedListener {
+                    buttonState.value = MapPointerState.DRAGGING
+                }
+                it.setOnCameraIdleListener {
+                    buttonState.value = MapPointerState.IDLE
+                }
             }
         }
     }
@@ -66,11 +81,11 @@ fun HomeScreen() {
 //TODO animate on drag
 //TODO remove click ripple
 @Composable
-fun MapPointer(modifier: Modifier = Modifier, onClick: () -> Unit) {
+fun MapPointer(modifier: Modifier = Modifier, state: TransitionState?, onClick: () -> Unit) {
     Stack(modifier) {
         Box(
             shape = CircleShape, backgroundColor = Color.Gray.copy(alpha = .3f),
-            modifier = Modifier.size(24.dp).gravity(Alignment.BottomCenter)
+            modifier = Modifier.size(state!![width]).gravity(Alignment.BottomCenter)
         )
         Image(
             asset = imageResource(id = R.drawable.ic_location_start_pointer),
@@ -82,19 +97,57 @@ fun MapPointer(modifier: Modifier = Modifier, onClick: () -> Unit) {
 
 }
 
-@Preview
-@Composable
-fun MapPointerPreview() {
-    SnappComposeTheme {
-        MapPointer {
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SnappComposeTheme {
-//        HomeScreen()
+        HomeScreen()
+    }
+}
+
+enum class MapPointerState {
+    IDLE, DRAGGING
+}
+
+val width = DpPropKey()
+
+@Preview
+@Composable
+fun AnimatedMapPointer(
+    modifier: Modifier = Modifier,
+    buttonState: State<MapPointerState> = mutableStateOf(MapPointerState.IDLE),
+    onClick: () -> Unit = {}
+) {
+    val transitionDefinition = transitionDefinition<MapPointerState> {
+        state(MapPointerState.IDLE) {
+            this[width] = 24.dp
+        }
+        state(MapPointerState.DRAGGING) {
+            this[width] = 32.dp
+        }
+        transition(fromState = MapPointerState.IDLE, toState = MapPointerState.DRAGGING) {
+            width using tween(durationMillis = 200)
+        }
+
+        transition(MapPointerState.DRAGGING to MapPointerState.IDLE) {
+            width using tween(durationMillis = 200)
+        }
+    }
+    val toState = if (buttonState.value == MapPointerState.IDLE) {
+        MapPointerState.DRAGGING
+    } else {
+        MapPointerState.IDLE
+    }
+
+    val state = transition(
+        definition = transitionDefinition,
+        initState = buttonState.value,
+        toState = toState
+    )
+
+    MapPointer(modifier = modifier, state = state) {
+        onClick()
+
     }
 }
