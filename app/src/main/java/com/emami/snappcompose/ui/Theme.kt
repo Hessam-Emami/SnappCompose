fe55186027f1572bc2f8a116b13af8f371f3115f
Hessam Emami <hesam.emami9@gmail.com>
2020-10-03 00:59:28 +0330

    Clean up resources

--- app/src/main/java/com/emami/snappcompose/ui/Theme.kt
+++ app/src/main/java/com/emami/snappcompose/ui/Theme.kt
@@ -5,40 +5,25 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-        primary = purple200,
-        primaryVariant = purple700,
-        secondary = teal200
-)
 
 private val LightColorPalette = lightColors(
-        primary = purple500,
-        primaryVariant = purple700,
-        secondary = teal200
+    primary = Color.White,
+    primaryVariant = Color.White,
+    secondary = Color.White
 
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
-fun SnappComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun SnappComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
     MaterialTheme(
-            colors = colors,
-            typography = typography,
-            shapes = shapes,
-            content = content
+        colors = LightColorPalette,
+        typography = typography,
+        shapes = shapes,
+        content = content
     )
 }
